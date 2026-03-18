e93d6fd48472c0135827f483487bcb9e647bdc98
MaksRozanski <s35362@pjwstk.edu.pl>
2026-03-18 11:28:51 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
-
 public class Main {
     public static void main(String[] args) {
+        Książka harryPotter = new Książka("Harry Potter", "J.K. Rowling", 3000, true);
 
-        }
-    }
+    harryPotter.wpisz_info();
+    harryPotter.wypozycz();
+    harryPotter.zwrot();
+}
 }
